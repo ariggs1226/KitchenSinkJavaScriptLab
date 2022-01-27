981ac9d70f7fbda8076f0fe1227af21c5a32f383
Aric Riggs <ariggs1226@gmail.com>
2022-01-27 09:34:12 -0600

    finished the JSKS lab docs.

---
+++ app.js
@@ -0,0 +1,70 @@
+let fullName = 'Aric Riggs';
+//Naming myself.
+const numberOfStates = 50;
+//States in the current Union.
+
+let quickMaths = 5 + 4;
+//Number six on the lab asked for this, so I provided.
+function sayHello(hey) {
+    alert(hey)
+}
+
+sayHello('Hello World!');
+
+function checkAge(name, age) {
+    if (age < 21) {
+        alert("Sorry " + name + ", you aren't old enough to view this page!")
+    }
+}
+
+checkAge('Charles', 21);
+
+checkAge('Abby', 27);
+
+checkAge('James',18);
+
+checkAge('John', 17);
+
+let favVege = ['Carrots', 'Onions', 'Potatoes', 'Broccoli']
+
+let x = 0
+
+while (x < 4) {
+    console.log(favVege[x])
+    x++;
+}
+
+let pet = {
+    name: 'Brownie',
+    breed: 'Chocolate Lab/Mixed'
+}
+
+console.log(pet.name + ' is a ' + pet.breed + '!')
+
+let namesAges = [age1, age2, age3, age4, age5]
+
+let age1 = {
+    name = 'Charlie',
+    age = 16
+}
+
+let age2 = {
+    name = 'Amy',
+    age = 22
+}
+
+let age3 = {
+    name = 'Luna',
+    age = 25
+}
+
+let age4 = {
+    name = 'Jeremy',
+    age = 19
+}
+
+let age5 = {
+    name = 'Aric',
+    age = 24
+}
+
